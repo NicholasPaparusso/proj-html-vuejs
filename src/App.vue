d58e22c12aa9398a85c4c5c955ed40b330db578a
NicholasPaparusso <nicopaparusso@gmail.com>
2022-11-27 12:01:08 +0100

    [add] Footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,17 @@
 import AppJumbo from './components/AppJumbo.vue';
 import AppWidget from './components/AppWidget.vue';
 import AppStats from './components/AppStats.vue';
+import AppFooter from './components/AppFooter.vue';
+import AppAboutMe from './components/AppAboutMe.vue';
 export default {
 
 components:{
 AppJumbo,
 AppWidget,
 AppStats,
+AppAboutMe,
+
+AppFooter
 },
 
 }
@@ -17,6 +22,8 @@ AppStats,
   <AppJumbo/>
   <AppWidget/>
   <AppStats/>
+  <AppAboutMe/>
+  <AppFooter/>
 </template>
 
 <style lang="scss">
